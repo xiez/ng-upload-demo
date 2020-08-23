a5d5ad2358a5c638ef0099be4d19db5661f49529
zhengxie <xiez1989@gmail.com>
2020-08-23 22:13:59 +0800

    wip: fe upload to be

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+import { UploadService } from './upload.service';
+
+interface HTMLInputEvent extends Event {
+    target: HTMLInputElement & EventTarget;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +14,51 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+
+    form: FormGroup;
+  response;
+  imageURL;
+
+    constructor(
+        private formBuilder: FormBuilder,
+        private uploadService: UploadService
+    ) { }
+
+    ngOnInit(): void {
+      console.log('111111111')
+      this.form = this.formBuilder.group({
+          profile: ['']
+      })
+    }
+
+    onChange(event) {
+      console.log(event)
+        if (event.target.files.length > 0) {
+            const file = event.target.files[0];
+            this.form.get('profile').setValue(file);
+        }
+  }
+
+    onSubmit() {
+      console.log('submit');
+        const formData = new FormData();
+        formData.append('file', this.form.get('profile').value);
+        formData.append('foo', 'bar'); // TEST
+
+    this.uploadService.upload(formData).subscribe(
+      (res) => {
+        this.response = res;
+        this.imageURL = `http://localhost:4200${res.file}`;
+        console.log(res);
+        console.log(this.imageURL);
+      },
+      (err) => {  
+        console.log(err);
+      }
+    );
+
+    }
+
+
+
 }
